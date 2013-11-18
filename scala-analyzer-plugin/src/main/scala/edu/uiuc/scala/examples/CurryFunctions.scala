f80d01b20b0cf1ab6194a378f4820e0c03fd21e0
Jing Vergara <vergara5@linux-a3.ews.illinois.edu>
2013-11-18 02:15:35 -0600

    Add another way of currying

--- scala-analyzer-plugin/src/main/scala/edu/uiuc/scala/examples/CurryFunctions.scala
+++ scala-analyzer-plugin/src/main/scala/edu/uiuc/scala/examples/CurryFunctions.scala
@@ -2,10 +2,12 @@ package edu.uiuc.scala.examples
 
 object CurryFunctions {
 
+  def fullsum(a: Int, b: Int) = a + b
+
   def sum(a: Int)(b: Int) = a + b
-  
+
   def add(a: Int): Int => Int = b => a + b
-  
+
   def main(args: Array[String]) {
     val f = sum(5)_
     println(f(10)) //15
@@ -14,5 +16,10 @@ object CurryFunctions {
 
     val g = add(5)
     println(g(5)) //10
+
+    val addCurried = (fullsum(_,_)).curried
+
+    println(fullsum(1, 2)) // 3
+    println(addCurried(2)(2)) // 4
   }
 }
