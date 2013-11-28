dcff2b454d7bb595ced957f183352e1c244389ed
Ubuntu <ubuntu@ip-10-184-20-13.ec2.internal>
2013-11-28 07:27:59 +0000

    Update aggregator script

--- scala-analyzer-plugin/aggregate.sh
+++ scala-analyzer-plugin/aggregate.sh
@@ -1,3 +1,3 @@
-~/tools/scala-2.10.3/bin/scalac src/main/scala/edu/uiuc/scala/plugin/StatsAggregator.scala 
-~/tools/scala-2.10.3/bin/scala edu.uiuc.scala.plugin.StatsAggregator
+scalac src/main/scala/edu/uiuc/scala/plugin/StatsAggregator.scala 
+scala edu.uiuc.scala.plugin.StatsAggregator
 
