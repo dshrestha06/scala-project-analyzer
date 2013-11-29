09464ae04cced07a0362953abb2b256255f64cf8
Jing Vergara <vergara5@linux-a1.ews.illinois.edu>
2013-11-29 17:23:12 -0600

    Create reports

--- scala-analyzer-plugin/src/main/scala/edu/uiuc/scala/plugin/StatsAggregator.scala
+++ scala-analyzer-plugin/src/main/scala/edu/uiuc/scala/plugin/StatsAggregator.scala
@@ -2,17 +2,21 @@ package edu.uiuc.scala.plugin
 
 import scala.io.Source
 import java.io.File
+import java.io.PrintWriter
+import java.io.FileWriter
 
 object StatsAggregator {
 
+  // map of feature usage and count for all projects
   val statsCounter = collection.mutable.Map[String, Int]()
-  val files = collection.mutable.Set[String]()
-  var totalNumberOfProjects:Int = 0
-  var fileName:String = ""
 
-  def count(text: String) {
+  val files = collection.mutable.Set[String]()
+  var totalNumberOfProjects: Int = 0
+  var fileName: String = ""
+
+  def countPerProject(text: String) {
     val split = text.split(",")
-    val key = split(1).trim
+    val key = fileName + ", " + split(1).trim
     files += fileName + split(0)
     if (statsCounter.contains(key)) {
       statsCounter(key) += split(2).trim.toInt
@@ -21,16 +25,50 @@ object StatsAggregator {
     }
   }
 
-  def main(args: Array[String]) {
+  def countAllProjects(text: String) {
+    val split = text.split(",")
+    val key = split(1).trim
+    if (statsCounter.contains(key)) {
+      statsCounter(key) += split(2).trim.toInt
+    } else {
+      statsCounter(key) = split(2).trim.toInt
+    }
+  }
+
+  def flush(file: File) {
+    val p = new PrintWriter(new FileWriter(file, true))
+
+    p.println(statsCounter.mkString("\n").replaceAll(" -> ", ", "))
+    p.flush
+    p.close
+  }
+
+  def printPerProject {
+    val outputFile = new File("output/project.csv")
+    outputFile.delete()
     for (file <- new File(System.getProperty("user.home") + "/output/").listFiles) {
       totalNumberOfProjects += 1
       fileName = file.getName
-      Source.fromFile(file).getLines.filter(_.split(",").length > 1).foreach(count)
+      statsCounter.clear
+      Source.fromFile(file).getLines.filter(_.split(",").length > 1).foreach(countPerProject)
+      flush(outputFile)
     }
+  }
+
+  def printAllProjects {
+    val outputFile = new File("output/allprojects.csv")
+    outputFile.delete()
+    statsCounter.clear
+    Source.fromFile(new File("output/project.csv")).getLines.filter(_.split(",").length > 1).foreach(countAllProjects)
+    flush(outputFile)
+  }
+
+  def main(args: Array[String]) {
+    new File("output").mkdir();
+    printPerProject
+    printAllProjects
     println("Total number of projects: " + totalNumberOfProjects)
     println("Total number of files: " + files.size)
-    println("Feature Usage:")
-    println(statsCounter.mkString("\n").replaceAll(" -> ", ": "))
   }
 
 }
